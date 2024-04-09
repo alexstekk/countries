02f17583fcc58ad12b5105da77de38ed7609cc43
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-04-09 17:11:59 +0300

    Basic logic complete

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,20 +3,45 @@ import { IoArrowBack } from 'react-icons/io5';
 
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
-
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import {
+	selectCurrentCountry,
+	selectDetails,
+} from '../store/details/details-selectors';
+import { useEffect } from 'react';
+import {
+	clearDetails,
+	loadCountryByName,
+} from '../store/details/details-actions';
 
 export const Details = () => {
-  const { name } = useParams();
-  const navigate = useNavigate();
+	const { name } = useParams();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const currentCountry = null;
+	const { currentCountry, error, status } = useSelector(selectDetails);
 
-  return (
-    <div>
-      <Button onClick={() => navigate(-1)}>
-        <IoArrowBack /> Back
-      </Button>
-      {currentCountry && <Info push={navigate} {...currentCountry} />}
-    </div>
-  );
+	useEffect(() => {
+		dispatch(loadCountryByName(name));
+		return () => {
+			dispatch(clearDetails());
+		};
+	}, [name, dispatch]);
+
+	return (
+		<div>
+			<Button onClick={() => navigate(-1)}>
+				<IoArrowBack /> Back
+			</Button>
+			{status === 'loading' && <h2>Loading...</h2>}
+			{error && <h2>{error}</h2>}
+			{currentCountry && (
+				<Info
+					push={navigate}
+					{...currentCountry}
+				/>
+			)}
+		</div>
+	);
 };
--- src/store/controls/controls-reducer.js
+++ src/store/controls/controls-reducer.js
@@ -1,4 +1,4 @@
-import { SET_REGION, SET_SEARCH } from './controls-actions';
+import { CLEAR_CONTROLS, SET_REGION, SET_SEARCH } from './controls-actions';
 
 const initialState = {
 	search: '',
@@ -7,6 +7,9 @@ const initialState = {
 
 export const controlsReducer = (state = initialState, { type, payload }) => {
 	switch (type) {
+		case CLEAR_CONTROLS: {
+			return initialState;
+		}
 		case SET_SEARCH: {
 			return {
 				...state,
